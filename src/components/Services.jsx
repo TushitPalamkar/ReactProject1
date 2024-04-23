b5ac91ea53eb930ecad6a6eedbaff6d0e8fd07d0
TushitPalamkar <143091687+TushitPalamkar@users.noreply.github.com>
2024-04-23 21:13:37 +0530

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,7 +5,7 @@ import img2 from "../assets/4.jpg"
 export default function Services(){
     return(
         <>
-        <div className="services">Services</div>
+        <div className="services"></div>
         <Carousel infiniteLoop autoPlay showStatus={false} showArrows={false} interval={1000} showThumbs={false}>
         <div>
             <img src={img1}alt="Image1" />
